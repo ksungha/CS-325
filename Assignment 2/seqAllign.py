375c98381ec2379912fee8298186997fa1c5ebcf
richardeg <richarde@oregonstate.edu>
2017-02-19 17:53:39 -0800

    added fuunctions to calculate cost

--- Assignment 2/seqAllign.py
+++ Assignment 2/seqAllign.py
@@ -4,6 +4,65 @@ import collections
 import sys
 import getopt
 
+def costDelete(data, x, costMatrix, n):  #Calculates the cost of aligning i with -
+	if(data[x].left[n] == 'A')
+		return costMatrix[1].dash
+	else if(data[x].left[n] == 'T')
+		return costMatrix[2].dash
+	else if(data[x].left[n] == 'G')
+		return costMatrix[3].dash
+	else if(data[x].left[n] == 'C')
+		return costMatrix[4].dash
+
+def costInsert(data, x, costMatrix, m):	 #Calculates the cost of aligning j with -
+	if(data[x].right[m] == 'A')
+		return costMatrix[1].dash
+	else if(data[x].right[m] == 'T')
+		return costMatrix[2].dash
+	else if(data[x].right[m] == 'G')
+		return costMatrix[3].dash
+	else if(data[x].right[m] == 'C')
+		return costMatrix[4].dash
+
+def costAlign(data, x, costMatrix, n, m):  #Calculates the cost of aligning i with j
+	if(data[x].left[n] == 'A')
+		if(data[x].right[m] == 'A')
+			return costMatrix[1].a
+		else if(data[x].right[m] == 'T')
+			return costMatrix[1].t
+		else if(data[x].right[m] == 'G')
+			return costMatrix[1].g
+		else if(data[x].right[m] == 'C')
+			return costMatrix[1].c
+	else if(data[x].left[n] == 'T')
+		if(data[x].right[m] == 'A')
+			return costMatrix[2].a
+		else if(data[x].right[m] == 'T')
+			return costMatrix[2].t
+		else if(data[x].right[m] == 'G')
+			return costMatrix[2].g
+		else if(data[x].right[m] == 'C')
+			return costMatrix[2].c
+	else if(data[x].left[n] == 'G')
+		if(data[x].right[m] == 'A')
+			return costMatrix[3].a
+		else if(data[x].right[m] == 'T')
+			return costMatrix[3].t
+		else if(data[x].right[m] == 'G')
+			return costMatrix[3].g
+		else if(data[x].right[m] == 'C')
+			return costMatrix[3].c
+	else if(data[x].left[n] == 'C')
+		if(data[x].right[m] == 'A')
+			return costMatrix[4].a
+		else if(data[x].right[m] == 'T')
+			return costMatrix[4].t
+		else if(data[x].right[m] == 'G')
+			return costMatrix[4].g
+		else if(data[x].right[m] == 'C')
+			return costMatrix[4].c
+
+
 def main(argv):
 	inputDataFile = ""
 	inputMatrixFile = ""
@@ -56,9 +115,14 @@ def main(argv):
 
 	#ALGORITHM HERE
 	for x in data:
+		D = []
+		for i in data[x].left:
+			D[i][0] = i
+		for j in data[x].right:
+			D[0][j] = j
 		for i in data[x].left:
 			for j in data[x].right:
-				
+				D[i][j] = min(D[i-1][j] + costDelete(data, x, costMatrix, i), D[i][j-1] + costInsert(data, x, costMatrix, j), D[i-1][j-1] + costAlign(data, x, costMatrix, i, j))
 
 
 if __name__ == "__main__":
