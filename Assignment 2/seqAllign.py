9f8b8d1f584bc6f75e6959207186b7107dd65051
jackson-wells <welljack@oregonstate.edu>
2017-02-20 18:00:04 -0800

    ITS WORKINGGGGG

--- Assignment 2/seqAllign.py
+++ Assignment 2/seqAllign.py
@@ -13,6 +13,7 @@ def costDelete(data, x, costMatrix, n):  #Calculates the cost of aligning i with
 		return costMatrix[3].dash
 	elif(data[x].left[n] == 'C'):
 		return costMatrix[4].dash
+	else: return 0
 
 def costInsert(data, x, costMatrix, m):	 #Calculates the cost of aligning j with -
 	if(data[x].right[m] == 'A'):
@@ -23,45 +24,56 @@ def costInsert(data, x, costMatrix, m):	 #Calculates the cost of aligning j with
 		return costMatrix[3].dash
 	elif(data[x].right[m] == 'C'):
 		return costMatrix[4].dash
+	else: return 0
 
 def costAlign(data, x, costMatrix, n, m):  #Calculates the cost of aligning i with j
+	return 9
 	if(data[x].left[n] == 'A'):
 		if(data[x].right[m] == 'A'):
-			return costMatrix[1].a
+			return int(costMatrix[1].a)
 		elif(data[x].right[m] == 'T'):
-			return costMatrix[1].t
+			return int(costMatrix[1].t)
 		elif(data[x].right[m] == 'G'):
-			return costMatrix[1].g
+			return int(costMatrix[1].g)
 		elif(data[x].right[m] == 'C'):
-			return costMatrix[1].c
+			return int(costMatrix[1].c)
 	elif(data[x].left[n] == 'T'):
 		if(data[x].right[m] == 'A'):
-			return costMatrix[2].a
+			return int(costMatrix[2].a)
 		elif(data[x].right[m] == 'T'):
-			return costMatrix[2].t
+			return int(costMatrix[2].t)
 		elif(data[x].right[m] == 'G'):
-			return costMatrix[2].g
+			return int(costMatrix[2].g)
 		elif(data[x].right[m] == 'C'):
-			return costMatrix[2].c
+			return int(costMatrix[2].c)
 	elif(data[x].left[n] == 'G'):
 		if(data[x].right[m] == 'A'):
-			return costMatrix[3].a
+			return int(costMatrix[3].a)
 		elif(data[x].right[m] == 'T'):
-			return costMatrix[3].t
+			return int(costMatrix[3].t)
 		elif(data[x].right[m] == 'G'):
-			return costMatrix[3].g
+			return int(costMatrix[3].g)
 		elif(data[x].right[m] == 'C'):
-			return costMatrix[3].c
+			return int(costMatrix[3].c)
 	elif(data[x].left[n] == 'C'):
 		if(data[x].right[m] == 'A'):
-			return costMatrix[4].a
+			return int(costMatrix[4].a)
 		elif(data[x].right[m] == 'T'):
-			return costMatrix[4].t
+			return int(costMatrix[4].t)
 		elif(data[x].right[m] == 'G'):
-			return costMatrix[4].g
+			return int(costMatrix[4].g)
 		elif(data[x].right[m] == 'C'):
-			return costMatrix[4].c
-
+			return int(costMatrix[4].c)
+	elif(data[x].left[n] == '-'):
+                if(data[x].right[m] == 'A'):
+                        return int(costMatrix[0].a)
+                elif(data[x].right[m] == 'T'):
+                        return int(costMatrix[0].t)
+                elif(data[x].right[m] == 'G'):
+                        return int(costMatrix[0].g)
+                elif(data[x].right[m] == 'C'):
+                        return int(costMatrix[0].c)
+	else: return int(0)
 
 def main(argv):
 	inputDataFile = ""
@@ -106,7 +118,7 @@ def main(argv):
 			continue
 		line.strip()
 		temp = line.split(',')
-		costMatrix.append(costStruct(temp[1],temp[2],temp[3],temp[4],temp[5]))
+		costMatrix.append(costStruct(int(temp[1]),int(temp[2]),int(temp[3]),int(temp[4]),int(temp[5])))
 	f.close()
 	# matrix data accesed via costMatrix[x].dash,costMatrix[x].a and so on
 	# costMatrix[0] is cost values for -
@@ -125,8 +137,13 @@ def main(argv):
 			D[0][j] = j
 		for i in range(seqLengths[seq]):
 			for j in range(seqLengths[seq+1]):
-				D[i][j] = min(D[i-1][j] + costDelete(data, x, costMatrix, i), D[i][j-1] + costInsert(data, x, costMatrix, j), D[i-1][j-1] + costAlign(data, x, costMatrix, i, j))
-		print D[len(data[x].left)][len(data[x].right)]
+				D1 = D[i-1][j] + int(costDelete(data, x, costMatrix, i))
+				D2 = D[i][j-1] + int(costInsert(data, x, costMatrix, j))
+				D3 = D[i-1][j-1] + int(costAlign(data, x, costMatrix, i, j))
+				DT = min(D1,D2)
+				DF = min(D3,DT)
+				D[i][j] = DF
+		print D[len(data[x].left)-1][len(data[x].right)-1]
 		seq = seq +2
 
 
