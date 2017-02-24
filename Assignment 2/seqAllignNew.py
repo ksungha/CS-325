86767fa5caac58847bf8f4c8861330ec892523d9
richardeg <richarde@oregonstate.edu>
2017-02-23 20:03:24 -0800

    it works totally good and stuff

--- Assignment 2/seqAllignNew.py
+++ Assignment 2/seqAllignNew.py
@@ -74,54 +74,38 @@ def editDistance(str1, str2, costMatrix):
 	m = len(str1)
 	n = len(str2)
 
-	print str1,'\n',str2
-	#print m,'\n',n
 	tbl = {}
 	tbl[0, 0] = 0
 	bt = {}
         bt[0,0] = 0
-	for i in range(1, m+1): tbl[i, 0] = tbl[i-1, 0] + int(costDelete(str1, costMatrix, i-1))
-	for j in range(1, n): tbl[0, j] = tbl[0, j-1] + int(costInsert(str2, costMatrix, j-1))
 	for i in range(1, m+1):
-		for j in range(1, n):
+		tbl[i, 0] = tbl[i-1, 0] + int(costDelete(str1, costMatrix, i-1))
+		bt[i, 0] = "D"
+	for j in range(1, n+1):
+		tbl[0, j] = tbl[0, j-1] + int(costInsert(str2, costMatrix, j-1))
+		bt[0, j] = "I"
+	for i in range(1, m+1):
+		for j in range(1, n+1):
 			val1 = tbl[i-1, j] + int(costDelete(str1, costMatrix, i-1))
 			val2 = tbl[i, j-1] + int(costInsert(str2, costMatrix, j-1))
 			val3 = tbl[i-1, j-1] + int(costAlign(str1, str2, costMatrix, i-1, j-1))
 			tbl[i, j] = min(val1, val2, val3)
 
 
-			if(tbl[i, j] == val2):
-                                bt[i,j] = "I"
-			elif(tbl[i, j] == val1):
+			if(tbl[i, j] == val1):
                                 bt[i,j] = "D"
+			elif(tbl[i, j] == val2):
+                                bt[i,j] = "I"
                         else:
                                 bt[i,j] = "A"
-			#print tbl[i, j],
-		#print '\n'
-	#print i,'\n',j
-	#print str1[i], str2[j]
-	#print tbl[i-1, j], tbl[i, j-1], tbl[i-1, j-1]
 	return tbl[i, j], bt
 	
 
 ##############MAIN##################
 
 def main(argv):
-	inputDataFile = ""
-	inputMatrixFile = ""
-	try:
-      		opts, args = getopt.getopt(argv,"hi:m:",["ifile=","mfile="])
-	except getopt.GetoptError:
-	      	print 'usage: seqAllign.py -i <input file> -m <cost matrix file>'
-      		sys.exit(2)
-   	for opt, arg in opts:
-      		if opt == '-h':
-			print 'seqAllign.py -i <input file> -m <cost matrix file>'
-         		sys.exit()
-      		elif opt in ("-i", "--ifile"):
-         		inputDataFile = arg
-      		elif opt in ("-m", "--mfile"):
-         		inputMatrixFile = arg
+	inputDataFile = "imp2input.txt"
+	inputMatrixFile = "imp2cost.txt"
 	if inputMatrixFile:
                 f = open(inputMatrixFile,'r')
         else:
@@ -149,42 +133,62 @@ def main(argv):
 	else:
 		sys.exit(2)
 	seqLengths = []
+	outfile = open('imp2output.txt', 'w')
 	for line in f:
-		minseq = ''
+		minseq1 = ''
+		minseq2 = ''
 		tracestr = ''
 		bt = {}
 		line.rstrip()
 		temp = line.split(',')
 		seq1 = temp[0]
 		seq2 = temp[1]
-		med, bt = editDistance(seq1, seq2, costMatrix)
-		m = len(seq1)-1
-		n = len(seq2)-1
-		while(m >= 0):
+		seq2 = seq2[:-1]
+		med, bt = editDistance(seq1, seq2, costMatrix)		#editDistance returns min edit dist and backtrace table
+		m = len(seq1)
+		n = len(seq2)
+		while(m > 0 or n > 0):			#loops through backtrace to build the backtrace string
 			if(bt[m, n] == 'I'):
 				n -= 1
 				tracestr = 'I' + tracestr
 			elif(bt[m, n] == 'D'):
 				m -= 1
 				tracestr = 'D' + tracestr
-			else:
+			elif(bt[m, n] == 'A'):
 				m -= 1
 				n -= 1
 				tracestr = 'A' + tracestr
+		if(m==0 and n!=0):			#if n or m doesn't reach 0, loops until they are both 0
+			for i in range(n):
+				n -= 1
+				tracestr = 'I' + tracestr
+		elif(n==0 and m!=0):
+			for i in range(m):		
+				m -= 1
+				tracestr = 'D' + tracestr
 		j = 0
-		for i in range(len(tracestr)):
+		for i in range(0,len(tracestr)):	#builds the first edited string using the backtrace string
 			if(tracestr[i] == 'I'):
-				minseq = minseq + '-'
+				minseq1 = minseq1 + '-'
 			elif(tracestr[i] == 'D'):
-				minseq = minseq + seq1[j]
+				minseq1 = minseq1 + seq1[j]
 				j += 1
 			else:
-				minseq = minseq + seq1[j]
+				minseq1 = minseq1 + seq1[j]
 				j += 1
-			
-		print minseq
-		print tracestr
-		break
+		
+		j = 0
+		for i in range(0,len(tracestr)):	#builds the second edited string
+			if(tracestr[i] == 'I'):
+				minseq2 = minseq2 + seq2[j]
+				j += 1
+			elif(tracestr[i] == 'D'):
+				minseq2 = minseq2 + '-'
+			else:
+				minseq2 = minseq2 + seq2[j]
+				j += 1	
+		final = minseq1 + ',' + minseq2 + ':' + str(med)
+		outfile.write(final+'\n')
 	f.close()
 	
 
