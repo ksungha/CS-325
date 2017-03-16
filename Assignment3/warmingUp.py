46ce6fbf7b4c2d11eba2d7a5f96cce31af2f718a
jackson-wells <welljack@oregonstate.edu>
2017-03-15 17:25:43 -0700

    no

--- Assignment3/warmingUp.py
+++ Assignment3/warmingUp.py
@@ -23,15 +23,15 @@ x4 = LpVariable("x4")
 x5 = LpVariable("x5")
 Td = LpVariable("Tdvar")
 prob += Td
-lin=0
+linear=0
 seasonal=0
 solar=0
 for i in range(0,n):
-	lin = (x0 + x1 * x_vals[i])
+	linear = (x0 + x1 * x_vals[i])
 	seasonal = (x2 * math.cos(2*math.pi * x_vals[i]/364.25) + x3 * math.sin(2*math.pi * x_vals[i]/364.25))
 	solar = (x4 * math.cos(2*math.pi * x_vals[i]/(364.25*10.7)) + x5 * math.sin(2*math.pi*x_vals[i]/(364.25 * 10.7)))
-	prob += Td >= (lin + seasonal + solar - y_vals[i])
-	prob += Td >= -(lin + seasonal + solar - y_vals[i])
+	prob += Td >= (linear + seasonal + solar - y_vals[i])
+	prob += Td >= -(linear + seasonal + solar - y_vals[i])
 status = prob.solve()
 print "\n\nObjective of LP:%f" %(value(prob.objective))
 print "Value of x0:%f" %(value(x0))
@@ -44,10 +44,10 @@ bestFit = []
 for i in range(n):
 	bestFit.append(value(x0)+value(x1)*x_vals[i]+value(x2)*math.cos(2*math.pi*x_vals[i]/364.25)+value(x3)*math.sin(2*math.pi*x_vals[i]/364.25)+value(x4)*math.cos(2*math.pi*x_vals[i]/(364.25*10.7))+value(x5)*math.sin(2*math.pi*x_vals[i]/(364.25 * 10.7)))
 
-linear = []
+lin = []
 for i in range(n):
-	linear.append(value(x0) + value(x1)*x_vals[i])
+	lin.append(value(x0) + value(x1)*x_vals[i])
 
-pPlot.plot(x_vals,y_vals, "bo", x_vals, bestFit, "r", x_vals, linear, "y", linewidth=5.0)
+pPlot.plot(x_vals,y_vals, "bo", x_vals, bestFit, "r", x_vals, lin, "y", linewidth=5.0)
 pPlot.axis([0,n,min_temp,max_temp])
 pPlot.show()
